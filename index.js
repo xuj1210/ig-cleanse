f50593616c59476337ea3d7e085bf63ec0c8c097
xuj1210 <joshuax1210@gmail.com>
2026-04-14 00:38:44 -0400

    fix for new following data shape

--- index.js
+++ index.js
@@ -13,51 +13,55 @@ followingInput.addEventListener("change", handleFiles, false);
 let MASTER_LISTS = {};
 
 function handleFiles() {
+  const followers = followersInput.files[0];
+  const following = followingInput.files[0];
+
+  if (followers && following) {
+    followersInput.removeEventListener("change", handleFiles, false);
+    followingInput.removeEventListener("change", handleFiles, false);
     const followers = followersInput.files[0];
     const following = followingInput.files[0];
-
-    if (followers && following) {
-        followersInput.removeEventListener("change", handleFiles, false);
-        followingInput.removeEventListener("change", handleFiles, false);
-        const followers = followersInput.files[0];
-        const following = followingInput.files[0];
-        readFile(followers);
-        readFile(following);
-    } else {
-        console.log("Missing a file");
-    }
+    readFile(followers);
+    readFile(following);
+  } else {
+    console.log("Missing a file");
+  }
 }
 
 function readFile(file) {
-    const reader = new FileReader();
-    reader.onload = handleJSON;
-    reader.readAsText(file);
+  const reader = new FileReader();
+  reader.onload = handleJSON;
+  reader.readAsText(file);
 }
 
 function handleJSON(event) {
-    const str = event.target.result;
-    const data = JSON.parse(str);
-    for (const prop in data) MASTER_LISTS[prop] = cleanList(data[prop]);
-    if (MASTER_LISTS.relationships_followers && MASTER_LISTS.relationships_following) {
-        MASTER_LISTS.relationships_followers = new Set(MASTER_LISTS.relationships_followers);
-        console.log('WE READY', MASTER_LISTS);
-        let diff = MASTER_LISTS.relationships_following.filter(x => !MASTER_LISTS.relationships_followers.has(x))
-        for (const name of diff) {
-            listDisplay.append(usernameToLI(name));
-        }
+  const str = event.target.result;
+  let data = JSON.parse(str);
+  if (Array.isArray(data)) {
+    data = { "relationships_followers": data };
+  }
+  for (const prop in data) MASTER_LISTS[prop] = cleanList(data[prop]);
+  if (MASTER_LISTS.relationships_followers && MASTER_LISTS.relationships_following) {
+    MASTER_LISTS.relationships_followers = new Set(MASTER_LISTS.relationships_followers);
+
+    for (const user of MASTER_LISTS.relationships_following) {
+      if (!MASTER_LISTS.relationships_followers.has(user)) {
+        listDisplay.append(usernameToLI(user));
+      }
     }
+  }
 }
 
 function usernameToLI(username) {
-    const user = String(username);
-    let ret = document.createElement("li");
-    ret.innerHTML = `<a target="_blank" href="https://instagram.com/${user}">${user}</a>`;
-    console.log(ret);
-    return ret;
+  const user = String(username);
+  let ret = document.createElement("li");
+  ret.innerHTML = `<a target="_blank" href="https://instagram.com/${user}">${user}</a>`;
+  return ret;
 }
 
 function cleanList(list) {
-    return list.map(val => {
-        return val.string_list_data[0].value;
-    });
+  return list.map(val => {
+    const href = val.string_list_data[0].href.trim();
+    return href.substring(href.lastIndexOf("/") + 1);
+  });
 }
